ff28345a8494778c72eb153be225e915a0007ae1
Kullanan Sunan <Kullananzaza@gmail.com>
2024-03-04 22:37:32 +0700

    nn-test

--- RestaurantEU/resources/views/customer/reservation/home.blade.php
+++ RestaurantEU/resources/views/customer/reservation/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Go Reservation</title>
     <link rel="stylesheet" href="{{ asset('css/output.css') }}">
 </head>
 <body class="bg-darkgreen flex justify-center items-center flex-col h-screen">
