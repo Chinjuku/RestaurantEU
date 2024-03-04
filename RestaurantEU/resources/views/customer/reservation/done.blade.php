639a1dbadbf90dac4b69aeb8ab5e75eaccec9a50
Kullanan Sunan <Kullananzaza@gmail.com>
2024-03-04 22:02:17 +0700

    nncommit-4

--- RestaurantEU/resources/views/customer/reservation/done.blade.php
+++ RestaurantEU/resources/views/customer/reservation/done.blade.php
@@ -10,13 +10,13 @@
 </head>
 <body class="bg-lightcream flex justify-center items-center flex-col -">
     <div class="flex justify-center items-center h-screen flex-col">
-        <div class="bg-darkgreen px-20 py-10 text- border rounded-3xl flex justify-center items-center flex-col absolute">
-            <h2 class="text-lightcream text-[32px] mb-5">การจองโต๊ะสำเร็จ</h2>
+        <div class="bg-darkgreen px-20 py-10 text-center border rounded-3xl flex justify-center items-center flex-col absolute">
+            <h2 class="text-lightcream text-[32px] mb-5 phone:text-[18px]">การจองโต๊ะสำเร็จ</h2>
             <a href="{{route('reservation.home')}}" class="bg-lightcream px-5 py-2 hover:bg-darkgreen hover:text-lightcream hover:transition duration-300 ease-in-out"><button type="submit">กลับสู่หน้าหลัก</button></a>
         </div>
         <div class="mt-[95%]">
-            <div class="flex justify-center items-center flex-row gap-6 text-darkgreen">
-            <svg width="30" height="30" viewBox="0 0 40 40" fill="0d4f4e" xmlns="http://www.w3.org/2000/svg">>
+            <div class="flex justify-center items-center flex-row gap-6 text-darkgreen phone:gap-3">
+            <svg width="30" height="30" viewBox="0 0 40 40" fill="0d4f4e" xmlns="http://www.w3.org/2000/svg" class="">
                 <g clip-path="url(#clip0_83_485)">
                 <path d="M30.9833 0H9.01667C4.03667 0 0 4.03667 0 9.01667V30.9833C0 35.9633 4.03667 40 9.01667 40H30.9833C35.9633 40 40 35.9633 40 30.9833V9.01667C40 4.03667 35.9617 0 30.9833 0ZM30.4733 24.775C27.675 27.995 21.42 31.9183 19.9967 32.5183C18.5733 33.1183 18.7833 32.1367 18.8417 31.7983C18.8767 31.5983 19.0317 30.6567 19.0317 30.6567C19.0767 30.3167 19.1233 29.7883 18.9883 29.4517C18.8383 29.08 18.2483 28.8883 17.8133 28.7933C11.405 27.9467 6.66 23.4667 6.66 18.1167C6.66 12.15 12.6433 7.29333 19.9967 7.29333C27.35 7.29333 33.3333 12.1483 33.3333 18.1167C33.3317 20.5033 32.4067 22.655 30.4733 24.775Z" fill="#0D4F4E"/>
                 <path d="M26.35 16.6864V17.6681H28.8966C29.0366 17.6681 29.1516 17.7831 29.1516 17.9231V18.8698C29.1516 19.0098 29.0383 19.1248 28.8966 19.1248H26.35V20.1064H28.8966C29.0366 20.1064 29.1516 20.2214 29.1516 20.3614V21.3081C29.1516 21.4481 29.0383 21.5614 28.8966 21.5614H25.15C25.01 21.5614 24.895 21.4481 24.895 21.3081V15.4864C24.895 15.3464 25.0083 15.2314 25.15 15.2314H28.8966C29.0366 15.2314 29.1516 15.3448 29.1516 15.4864V16.4314C29.1516 16.5714 29.0383 16.6864 28.8966 16.6864H26.35Z" fill="#0D4F4E"/>
@@ -30,14 +30,14 @@
                 </clipPath>
                 </defs>
             </svg>
-                <i class='bx bxl-facebook-circle bx-md' style='color:#0d4f4e' class="mx-6"></i>
-                <span class="flex justify-center items-center flex-row gap-5">
-                    <i class='bx bx-envelope bx-md' style='color:#0d4f4e'></i>
-                    <h5 class="text-[24px]">email@xxxx.com</h5>
+                <i class='bx bxl-facebook-circle bx-md' style='color:#0d4f4e' class="mx-6 phone:mx-3"></i>
+                <span class="flex justify-center items-center flex-row gap-5 phone:gap-1">
+                    <i class='bx bx-envelope bx-md phone:bx-xs' style='color:#0d4f4e'></i>
+                    <h5 class="text-[24px] phone:text-[12px]">email@xxxx.com</h5>
                 </span>
-                <span class="flex justify-center items-center flex-row gap-5">
-                    <i class='bx bx-phone-call bx-md' style='color:#0d4f4e'></i>
-                    <h5 class="text-[24px]">xxx-xxx-xxxx</h5>
+                <span class="flex justify-center items-center flex-row gap-5 phone:gap-1">
+                    <i class='bx bx-phone-call bx-md phone:bx-xs' style='color:#0d4f4e'></i>
+                    <h5 class="text-[24px] phone:text-[12px]">xxx-xxx-xxxx</h5>
                 </span>
                 
             </div>
--- RestaurantEU/resources/views/customer/reservation/form.blade.php
+++ RestaurantEU/resources/views/customer/reservation/form.blade.php
@@ -9,9 +9,9 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Document</title>
 </head>
-<body class="bg-lightcream grid grid-cols-2 ">
-    <div class="my-24 mx-24 text-3xl flex justify-between flex-col">
-        <div class="">
+<body class="bg-lightcream grid grid-cols-2 phone:grid phone:grid-cols-1 phone:w-[100%]">
+    <div class="my-24 mx-24 text-3xl flex justify-between flex-col phone:text-xl phone:flex phone:justify-start phone:flex-cols tablet:text-2xl">
+        <div class="phone:inline-block">
             <h2 class="mb-4">ติดต่อ : </h2>
             <div class="inline-flex gap-2 hover:underline duration-200">
                 <i class='bx bx-envelope'></i><p>email@xxx.com</p>
@@ -21,20 +21,21 @@
                 <p>xxx-xxx-xxx</p>
             </div>
         </div>
-        <div class="">
+        <div class="phone:inline-block">
             <i class='bx bxl-instagram' class="text-darkgreen"></i>
             <i class='bx bxl-facebook-circle' class="text-darkgreen"></i>
         </div>
     </div>
-    <div class="mt-5 bg-darkgreen rounded-3xl px-30 py-32">
+    <div class="mt-5 bg-darkgreen rounded-3xl px-30 py-32 phone:text-[16px] phone:mt-0 phone:px-10 phone:py-12
+    phone:">
         <div>
-            <h2 class="text-[48px] text-lightcream font-bold text-center">จองโต๊ะอาหาร</h2>
+            <h2 class="text-[48px] text-lightcream font-bold text-center phone:text-[20px] tablet:text-2xl">จองโต๊ะอาหาร</h2>
         </div>
         <div class="">
             <form action="/customer/reserving" method="POST">
                 @csrf
-                <div class="mx-auto w-4/5 flex flex-col px-5 py-3">
-                    <label class="text-lightcream text-[24px]">ชื่อ</label>
+                <div class="mx-auto w-4/5 flex flex-col px-5 py-3 phone:w-full">
+                    <label class="text-lightcream text-[24px] phone:text-[16px] tablet:text-l">ชื่อ</label>
                     <input type="text" name="name" placeholder="ชื่อ" value="{{ old('name') }}" class="text-white bg-transparent bg-transparent border-0 border-b-2 border-yellow-50">
                     @error('name')
                         <span class="text-red-600" role="alert">
@@ -42,8 +43,8 @@
                         </span>
                     @enderror
                 </div>
-                <div class=" mx-auto w-4/5 flex flex-col px-5 py-3">
-                    <label class="text-lightcream text-[24px]">เบอร์โทรศัพท์</label>
+                <div class=" mx-auto w-4/5 flex flex-col px-5 py-3 phone:w-full">
+                    <label class="text-lightcream text-[24px] phone:text-[16px] tablet:text-l">เบอร์โทรศัพท์</label>
                     <input type="text" name="phone_num" placeholder="เบอร์โทรศัพท์" value="{{ old('phone_num') }}" class="text-white bg-transparent border-0 border-b-2 border-yellow-50">
                     @error('phone_num')
                         <span class="text-red-600" role="alert">
@@ -51,8 +52,8 @@
                         </span>
                     @enderror
                 </div>
-                <div class="mx-auto w-4/5 flex flex-col px-5 py-3">
-                    <label class="text-lightcream text-[24px]">เวลา</label>
+                <div class="mx-auto w-4/5 flex flex-col px-5 py-3 phone:w-full">
+                    <label class="text-lightcream text-[24px] phone:text-[16px] tablet:text-l">เวลา</label>
                     <input type="time" name="time" value="{{ old('time') }}" class="text-white bg-transparent border-0 border-b-2 border-yellow-50 peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0">
                     @error('time')
                         <span class="text-red-600" role="alert">
@@ -60,8 +61,8 @@
                         </span>
                     @enderror
                 </div>
-                <div class="mx-auto w-4/5 flex flex-col px-5 py-3">
-                    <label class="text-lightcream text-[24px]">จำนวน</label>
+                <div class="mx-auto w-4/5 flex flex-col px-5 py-3 phone:w-full">
+                    <label class="text-lightcream text-[24px] phone:text-[16px] tablet:text-l">จำนวนคน</label>
                     <input type="text" name="people_num" placeholder="จำนวน" value="{{ old('phone_num') }}" class="text-white bg-transparent border-0 border-b-2 border-yellow-50">
                     @error('people_num')
                         <span class="text-red-600" role="alert">
@@ -70,7 +71,7 @@
                     @enderror
                 </div>
             
-                <button type="submit" class="mx-auto mt-3 flex items-center justify-end px-5 py-3 text-[24px] bg-lightcream hover:bg-darkgreen hover:text-lightcream hover:transition duration-400 ease-out" >ยืนยันการจอง</button>
+                <button type="submit" class="mx-auto mt-3 flex items-center justify-end px-5 py-3 text-[24px] bg-lightcream hover:bg-darkgreen hover:text-lightcream hover:transition duration-400 ease-out tablet:text-l" >ยืนยันการจอง</button>
             </form>
         </div>
     </div>
